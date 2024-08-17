ffe56e5967164243c439275f47825c0a2f83c5c6
Eduard Yapparov <shinoda0043@gmail.com>
2024-08-17 13:57:08 +0300

    Добавил ожидание элементов

--- Locators.py
+++ Locators.py
@@ -3,30 +3,32 @@ from selenium.webdriver.common.by import By
 
 class Locators:
     #
-    Reg_Name = (By.XPATH, "//label[text() = 'Имя']/following-sibling::input")  # Поле Имя
-    Reg_Password = (By.XPATH, "//label[text() = 'Пароль']/following-sibling::input")  # Поле Пароль
-    Reg_Email = (By.XPATH, "//label[text() = 'Email']/following-sibling::input")  # Поле Email
+    REG_NAME = (By.XPATH, "//label[text() = 'Имя']/following-sibling::input")  # Поле Имя
+    REG_PASSWORD = (By.XPATH, "//label[text() = 'Пароль']/following-sibling::input")  # Поле Пароль
+    REG_EMAIL = (By.XPATH, "//label[text() = 'Email']/following-sibling::input")  # Поле Email
     # Кнопки
-    Reg_button = (By.XPATH, "//button[text() = 'Зарегистрироваться']")  # Регистрация
-    Entry_button = (By.XPATH, "//button[text() = 'Войти']")  # Вход
-    Order_button = (By.XPATH, "//button[text() = 'Оформить заказ']")  # Оформление заказа
-    Profile_button = (By.XPATH, "//p[text() = 'Личный Кабинет']")  # Переход в личный кабинет
-    Constructor_button = (By.XPATH, "//p[text() = 'Конструктор']")  # Переход в конструктор
-    Logout_button = (By.XPATH, "//button[text() = 'Выход']")  # Выход
-    Buns_button = (By.XPATH, "//span[text() = 'Булки']/parent::div")  # Переход к булкам
-    Sauces_button = (By.XPATH, "//span[text() = 'Соусы']")  # Переход к соусам
-    Toppings_button = (By.XPATH, "//span[text() = 'Начинки']")  # Переход к начинкам
+    REG_BUTTON = (By.XPATH, "//button[text() = 'Зарегистрироваться']")  # Регистрация
+    ENTRY_BUTTON = (By.XPATH, "//button[text() = 'Войти']")  # Вход
+    ORDER_BUTTON = (By.XPATH, "//button[text() = 'Оформить заказ']")  # Оформление заказа
+    PROFILE_BUTTON = (By.XPATH, "//p[text() = 'Личный Кабинет']")  # Переход в личный кабинет
+    CONSTRUCTOR_BUTTON = (By.XPATH, "//p[text() = 'Конструктор']")  # Переход в конструктор
+    LOGOUT_BUTTON = (By.XPATH, "//button[text() = 'Выход']")  # Выход
+    BUNS_BUTTON = (By.XPATH, "//span[text() = 'Булки']/parent::div")  # Переход к булкам
+    SAUCES_BUTTON = (By.XPATH, "//span[text() = 'Соусы']")  # Переход к соусам
+    TOPPINGS_BUTTON = (By.XPATH, "//span[text() = 'Начинки']")  # Переход к начинкам
     # Ингридиенты
-    Buns = (By.XPATH, "//h2[text() = 'Булки']")  # Булки
-    Sauces = (By.XPATH, "//h2[text() = 'Соусы']")  # Соуса
-    Toppings = (By.XPATH, "//h2[text() = 'Начинки']")  # Начинки
+    BUNS = (By.XPATH, "//h2[text() = 'Булки']")  # Булки
+    SAUCES = (By.XPATH, "//h2[text() = 'Соусы']")  # Соуса
+    TOPPINGS = (By.XPATH, "//h2[text() = 'Начинки']")  # Начинки
 
-    Order_history = (By.XPATH, "//a[text() = 'История заказов']")  # История Заказов
-    Wrong_password_info = (By.XPATH, "//p[text() = 'Некорректный пароль']")  # Забыли пароль
-    Entrance = (By.XPATH, "//h2[text() = 'Вход']")  # Вход
-    Main_logo = (By.XPATH, "//div[@class = 'AppHeader_header__logo__2D0X2']")  # Логотип
-    Entrance_from_main = (By.XPATH, "//button[text() = 'Войти в аккаунт']")  # Войти в аккаунт
-    Entrance_from_profile = (By.XPATH, "//p[text() = 'Личный Кабинет']")  # Личный кабинет
-    Entrance_from_reg = (By.XPATH, "//a[text() = 'Войти']")   # Войти
-    Lost_Password = (By.XPATH, "//a[text() = 'Восстановить пароль']")  # Восстановить пароль
-    Entrance_from_lost_password = (By.XPATH, "//a[text() = 'Войти']")  # Вспомнили пароль
+    ORDER_HISTORY = (By.XPATH, "//a[text() = 'История заказов']")  # История Заказов
+    WRONG_PASSWORD_INFO = (By.XPATH, "//p[text() = 'Некорректный пароль']")  # Забыли пароль
+    ENTRANCE = (By.XPATH, "//h2[text() = 'Вход']")  # Вход
+    # MAIN_LOGO = (By.XPATH, "//div[@class = 'AppHeader_header__logo__2D0X2']")
+    MAIN_LOGO = (By.XPATH, "//div[contains(@class, 'AppHeader_header__logo')]")  # Логотип
+    ENTRANCE_FROM_MAIN = (By.XPATH, "//button[text() = 'Войти в аккаунт']")  # Войти в аккаунт
+    ENTRANCE_FROM_PROFILE = (By.XPATH, "//p[text() = 'Личный Кабинет']")  # Личный кабинет
+    ENTRANCE_FROM_REG = (By.XPATH, "//a[text() = 'Войти']")   # Войти
+    LOST_PASSWORD = (By.XPATH, "//a[text() = 'Восстановить пароль']")  # Восстановить пароль
+    ENTRANCE_FROM_LOST_PASSWORD = (By.XPATH, "//a[text() = 'Войти']")  # Вспомнили пароль
+
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -11,31 +11,31 @@ class TestRegistration:
 
         generated_email = Help.generate_email()
 
-        email_input = driver.find_element(*Locators.Reg_Email)
-        name_input = driver.find_element(*Locators.Reg_Name)
-        password_input = driver.find_element(*Locators.Reg_Password)
-        reg_button = driver.find_element(*Locators.Reg_button)
+        email_input = driver.find_element(*Locators.REG_EMAIL)
+        name_input = driver.find_element(*Locators.REG_NAME)
+        password_input = driver.find_element(*Locators.REG_PASSWORD)
+        reg_button = driver.find_element(*Locators.REG_BUTTON)
 
         email_input.send_keys(generated_email)
         name_input.send_keys(Data.AUTH_NAME)
         password_input.send_keys(Data.WRONG_PASSWORD)
         reg_button.click()
-        password_info = driver.find_element(*Locators.Wrong_password_info)
+        password_info = driver.find_element(*Locators.WRONG_PASSWORD_INFO)
         assert password_info.is_displayed()
 
     def test_registration(self, driver):
 
         generated_email = Help.generate_email()
 
-        email_input = driver.find_element(*Locators.Reg_Email)
-        name_input = driver.find_element(*Locators.Reg_Name)
-        password_input = driver.find_element(*Locators.Reg_Password)
-        reg_button = driver.find_element(*Locators.Reg_button)
+        email_input = driver.find_element(*Locators.REG_EMAIL)
+        name_input = driver.find_element(*Locators.REG_NAME)
+        password_input = driver.find_element(*Locators.REG_PASSWORD)
+        reg_button = driver.find_element(*Locators.REG_BUTTON)
 
         name_input.send_keys(Data.AUTH_NAME)
         email_input.send_keys(generated_email)
         password_input.send_keys(Data.AUTH_PASSWORD)
         reg_button.click()
-        WebDriverWait(driver, Data.WAIT_TIME).until(EC.visibility_of_element_located(Locators.Entrance))
-        entrance_stage = driver.find_element(*Locators.Entrance)
+        WebDriverWait(driver, Data.WAIT_TIME).until(EC.visibility_of_element_located(Locators.ENTRANCE))
+        entrance_stage = driver.find_element(*Locators.ENTRANCE)
         assert entrance_stage.is_displayed()
